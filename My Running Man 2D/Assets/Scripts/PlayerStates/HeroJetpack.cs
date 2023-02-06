8f25f60455f2385facfe19cb2f153ee4d4274a98
DatDT1 <datdthe151528@fpt.edu.vn>
2023-02-06 16:15:08 +0700

    add hero jet pack

---
+++ My Running Man 2D/Assets/Scripts/PlayerStates/HeroJetpack.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class HeroJetpack : HeroStates
+{
+    [Header("Settings")]
+    [SerializeField] private float jetpackForce = 3f;
+    [SerializeField] private float jetpackFuel = 5f;
+
+    public float _fuelLeft;
+    private float _fuelDurationLeft;
+    private bool _stillHaveFuel = true;
+
+    protected override void InitState()
+    {
+        base.InitState();
+        _fuelDurationLeft = jetpackFuel;
+        _fuelLeft = jetpackFuel;
+    }
+    protected override void GetInput()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            JetPack();
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            EndJetPack();
+        }
+    }
+    private void Update()
+    {
+        Debug.Log(_fuelLeft);
+    }
+
+    private void JetPack()
+    {
+        if (!_stillHaveFuel)
+        {
+            return;
+        }
+        if (_fuelLeft <= 0)
+        {
+            EndJetPack();
+            _stillHaveFuel = false;
+            return;
+        }
+        _playerController.SetVerticalForce(jetpackForce);
+        _playerController.Conditions.IsJetPacking = true;
+        StartCoroutine(BurnFuel());
+    }
+
+    private void EndJetPack()
+    {
+        _playerController.Conditions.IsJetPacking = false;
+        StartCoroutine(Refill());
+    }
+
+    private IEnumerator BurnFuel()
+    {
+        float fuelConsumed = _fuelLeft;
+        if (fuelConsumed > 0 && _playerController.Conditions.IsJetPacking && _fuelLeft <= fuelConsumed)
+        {
+            fuelConsumed -= Time.deltaTime;
+            _fuelLeft = fuelConsumed;
+            yield return null;
+        }
+    }
+
+    private IEnumerator Refill()
+    {
+        yield return new WaitForSeconds(0.5f);
+        float fuel = _fuelLeft;
+        while (fuel < jetpackFuel && !_playerController.Conditions.IsJetPacking)
+        {
+            fuel += Time.deltaTime;
+            _fuelLeft = fuel;
+
+            if (!_stillHaveFuel && fuel > 0.2f)
+            {
+                _stillHaveFuel = true;
+            }
+            yield return null;
+        }
+    }
+}
