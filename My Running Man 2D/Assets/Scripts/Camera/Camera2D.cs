827c84cf841712e13d1c6de5f7b956a68a3621c7
DatDT1 <datdthe151528@fpt.edu.vn>
2023-02-17 16:37:09 +0700

    camera follow hero

--- My Running Man 2D/Assets/Scripts/Camera/Camera2D.cs
+++ My Running Man 2D/Assets/Scripts/Camera/Camera2D.cs
@@ -22,8 +22,14 @@ public class Camera2D : MonoBehaviour
 
     public Vector3 TargetPosition { get; set; } //the actual position of hero
     public Vector3 CameraTargetPosition { get; set; } //the position camera know about
+
     private float _targetHorizontalSmoothFollow;
     private float _targetVerticalSmoothFollow;
+
+    private void Awake()
+    {
+        CenterOnTarger(playerToFollow);
+    }
     private void Update()
     {
         MoveCamera();
@@ -35,9 +41,31 @@ public class Camera2D : MonoBehaviour
         TargetPosition = GetHeroPosition(playerToFollow);
         CameraTargetPosition = new Vector3(TargetPosition.x, TargetPosition.y, 0f);
 
-        //Follow on selected axis
+        //Follow on selected axis (true/false)
+        float xPos = horizontalFollow ? CameraTargetPosition.x : transform.localPosition.x;
+        float yPos = verticalFollow ? CameraTargetPosition.y : transform.localPosition.y;
 
+        //Set offset (whether by horizontalFollow?)
+        CameraTargetPosition += new Vector3(horizontalFollow ? horizontalOffset : 0f
+                                            , verticalFollow ? verticalOffset : 0f, 0f);
+        //Set smooth value:
+        _targetHorizontalSmoothFollow = Mathf.Lerp(_targetHorizontalSmoothFollow,
+                                            CameraTargetPosition.x,
+                                            horizontalSmoothness * Time.deltaTime);
+        _targetVerticalSmoothFollow = Mathf.Lerp(_targetVerticalSmoothFollow,
+                                            CameraTargetPosition.y,
+                                            verticalSmoothness * Time.deltaTime);
+        //Get direction towards target pos
+        float xDirection = _targetHorizontalSmoothFollow - transform.localPosition.x;
+        float yDirection = _targetVerticalSmoothFollow - transform.localPosition.y;
 
+        Vector3 deltaDirection = new Vector3(xDirection, yDirection, 0f);
+
+        //New position (where the frame at...)
+        Vector3 newCameraPosition = transform.localPosition + deltaDirection;
+
+        //Apply new position 
+        transform.localPosition = new Vector3(newCameraPosition.x, newCameraPosition.y, transform.localPosition.z);
     }
 
     //get heros position
@@ -57,7 +85,20 @@ public class Camera2D : MonoBehaviour
     private void CenterOnTarger(HeroMotor player)
     {
         Vector3 targetPosition = GetHeroPosition(player);
+
+        //set smooth at the begining start
+        _targetHorizontalSmoothFollow = targetPosition.x;
+        _targetVerticalSmoothFollow = targetPosition.y;
+
         transform.localPosition = targetPosition; // set camera
+    }
+
+    //See camera center on screen 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 camPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 2f);
+        Gizmos.DrawWireSphere(camPosition, 0.5f);
     }
 
 }
