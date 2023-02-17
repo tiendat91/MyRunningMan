10adff73e7193280e2cdc6ddebafd50e5dc8a100
DatDT1 <datdthe151528@fpt.edu.vn>
2023-02-17 17:53:08 +0700

    camera shaking

--- My Running Man 2D/Assets/Scripts/Camera/CameraShake.cs
+++ My Running Man 2D/Assets/Scripts/Camera/CameraShake.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Settings")]
+    [SerializeField] private float shakeVibration = 10f;
+    [SerializeField] private float shakeRandomness = 0.2f;
+    [SerializeField] private float shakeTime = 0.01f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Shake();
+        }
+    }
+    public void Shake()
+    {
+        StartCoroutine(IEShake());
+    }
+    private IEnumerator IEShake()
+    {
+        Vector3 currentPos = transform.position;
+        for (int i = 0; i < shakeVibration; i++)
+        {
+            // set random position = current position + random position (with radius 1.0)
+            Vector3 shakePos = currentPos + Random.onUnitSphere * shakeRandomness;
+            yield return new WaitForSeconds(shakeTime);
+            transform.position = shakePos;
+        }
     }
 }
