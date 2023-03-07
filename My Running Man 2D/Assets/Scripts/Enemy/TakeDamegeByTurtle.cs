7f23c3bb86ade63d6377cda17f3b4824e098f2be
sontqhe153230 <91557584+sontqhe153230@users.noreply.github.com>
2023-03-07 16:46:47 +0700

    update

---
+++ My Running Man 2D/Assets/Scripts/Enemy/TakeDamegeByTurtle.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamegeByTurtle : MonoBehaviour
+{
+
+    [Header("State")]
+
+    [SerializeField] private AIState compareState;
+    public LayerMask playerMask;
+
+    Animator myAnimation;
+    public StateController myStateController;
+    bool PlayerActive = false;
+    bool isAttack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy enemy = myAnimation.GetComponent<Enemy>();
+        if (compareState == myStateController.currentState)
+        {
+            enemy.enabled = false;
+            Defense();
+            isAttack = true;
+        }
+        else
+        {
+            enemy.enabled = true;
+            isAttack = false;
+            Idle();
+        }
+    }
+    void Defense()
+    {
+        myAnimation.Play("turtle_attack");
+    }
+    void Idle()
+    {
+        myAnimation.Play("turtle_idle");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isAttack == true)
+        {
+            Debug.Log("LoseLife");
+            collision.GetComponent<Health>().LoseLife();
+        }
+
+    }
+}
---
+++ My Running Man 2D/Assets/Scripts/Enemy/TakeDmageByCrab.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDmageByCrab : MonoBehaviour
+{
+    [Header("State")]
+
+    [SerializeField] private AIState compareState;
+    public LayerMask playerMask;
+
+    Animator myAnimation;
+    public StateController myStateController;
+    bool PlayerActive = false;
+    bool isAttack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (compareState == myStateController.currentState)
+        {
+
+            Attack();
+            isAttack = true;
+        }
+        else
+        {
+            Idle();
+            isAttack = false;
+
+        }
+    }
+    void Attack()
+    {
+        myAnimation.Play("crab_attack");
+    }
+    void Idle()
+    {
+        myAnimation.Play("crab_idle");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isAttack == true)
+        {
+            Debug.Log("LoseLife");
+            collision.GetComponent<Health>().LoseLife();
+        }
+
+    }
+}
