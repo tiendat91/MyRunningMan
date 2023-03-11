f1cc7d9b316767fcd5b2d0eec82456b84bc26af4
sontqhe153230 <91557584+sontqhe153230@users.noreply.github.com>
2023-03-11 14:12:59 +0700

    update

--- My Running Man 2D/Assets/Scripts/UISpecialRound.cs
+++ My Running Man 2D/Assets/Scripts/UISpecialRound.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,23 +11,29 @@ public class UISpecialRound : MonoBehaviour
     [SerializeField] private GameObject panelSettings;
     [SerializeField] private GameObject optionsSettings;
     [SerializeField] private TextMeshProUGUI timeCount;
+    [SerializeField] private TextMeshProUGUI enemyKill;
     [SerializeField] public AudioSource _audio;
 
     [SerializeField] private float timeCounting = 10;
+    private float bonusTimeByEnemy = 5;
     [SerializeField] private bool timerIsRunning = false;
-
-    // Start is called before the first frame update
+    private int enemyCount = 0;
+    private int enemyKillCount = 0;
     void Start()
     {
         timerIsRunning = true;
-
         panelSettings.SetActive(false);
         optionsSettings.SetActive(false);
+        GameObject[] enemies = GameObject.FindObjectsOfType<GameObject>().Where(obj => obj.layer == LayerMask.NameToLayer("Enemy")).ToArray();
+        enemyCount = enemies.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int enemyCountUpdate = 0;
+        GameObject[] enemies = GameObject.FindObjectsOfType<GameObject>().Where(obj => obj.layer == LayerMask.NameToLayer("Enemy")).ToArray();
+        enemyCountUpdate = enemies.Length;
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             TurnOnPauseMenu(true);
@@ -34,22 +41,41 @@ public class UISpecialRound : MonoBehaviour
 
         if (timerIsRunning)
         {
-            if (timeCounting >= 0)
+            if (timeCounting > 0)
             {
-                timeCounting += Time.deltaTime;
+                timeCounting -= Time.deltaTime;
+                if (enemyCountUpdate < enemyCount)
+                {
+                    timeCounting += bonusTimeByEnemy;
+                    enemyKillCount += 1;
+                    enemyKill.text = "0" + enemyKillCount;
+                    enemyCount = enemyCountUpdate;
+                }
+
             }
             else
             {
                 Debug.Log("Time has run out!");
+                Debug.Log("GameOver");
+                SceneManager.LoadSceneAsync("Menu");
                 timeCounting = 0;
                 timerIsRunning = false;
             }
         }
-
+        if (enemyCountUpdate == 0)
+        {
+            SceneManager.LoadSceneAsync("Menu");
+            timeCounting = 0;
+            timerIsRunning = false;
+        }
+        if (GameObject.Find("Player(Clone)") == null)
+        {
+            SceneManager.LoadSceneAsync("Menu");
+            timeCounting = 0;
+            timerIsRunning = false;
+        }
         timeCount.text = TimeSpan.FromSeconds(timeCounting).ToString("hh':'mm':'ss");
-
     }
-
     public void TurnOnPauseMenu(bool isOn)
     {
         if (isOn)
